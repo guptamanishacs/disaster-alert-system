d03d873e5bd09e0294c9e568e9877da8bb592bf0
Manisha Gupta S080 <manisha@example.com>
2026-03-10 13:30:11 +0530

    update frontend

--- config.js
+++ config.js
@@ -1,3 +1,2 @@
 const API_URL = "https://disaster-alert-system-production.up.railway.app/api";
-
 export default API_URL;
---
+++ newFile.js
@@ -0,0 +1,3 @@
+import { API_URL } from "./config";
+
+export default API_URL;
